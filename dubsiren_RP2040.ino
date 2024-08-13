b7b53e3a49f9f0980b85f3cb4e09d8c123b85bbc
sdrum01 <sdrum01@gmail.com>
2024-08-13 21:49:06 +0200

    LFO Anpassungen

--- dubsiren_RP2040.ino
+++ dubsiren_RP2040.ino
@@ -21,7 +21,7 @@ const int controlPin = 2;  // Steuerpin für die Tonaktivierung
 const float minVal = 50.0;
 const float maxVal = 8000.0;
 
-float lfoFrequency = 100;  // LFO-Frequenz in Hz (0.5 Hz = 2 Sekunden Periode)
+unsigned int lfoFrequency = 1;  // LFO-Frequenz in Hz (0.5 Hz = 2 Sekunden Periode)
 float lfoAmplitude = 0;
 bool startButton = 1;
 
@@ -45,9 +45,16 @@ int pitchReadIndex = 0;
 int pitchTotal = 0;
 int valPitch = 0;
 
-void debug(String s){
+void debugFloat(float f){
   if (LOGLEVEL > 0){
-    Serial.println(s);
+
+    
+    char buffer[10];  // Puffer für die Zeichenkette, stelle sicher, dass er groß genug ist
+    dtostrf(f, 6, 4, buffer);  // 6 Zeichen insgesamt, 4 Dezimalstellen
+    String myString = String(buffer);
+
+
+    Serial.println(myString);
   }
 }
 
@@ -75,7 +82,7 @@ void setup() {
   }
   if(LOGLEVEL > 0){
     Serial.begin(115200);
-    debug("Setup abgeschlossen.");
+    //debug("Setup abgeschlossen.");
   }
 }
 
@@ -83,7 +90,8 @@ void setup() {
 
 // Funktion zur Umwandlung einer linearen Eingangsgröße in eine logarithmische Ausgabe
 float linearToLogarithmic(float percentage) {
-
+  
+  
   
   // Überprüfen, ob die Eingabewerte sinnvoll sind
   if (percentage < 0.0) percentage = 0.0;
@@ -108,8 +116,14 @@ float linearToLogarithmic(float percentage) {
 
 // LFO-Variante aus dem Arduino-Nano-Script
 float calculateLFOWave(float lfoFrequency, float lfoAmplitude) {
+
+  float schrittweite = lfoFrequency / 1000;
+  //float schrittweite = 0.01;
   unsigned long currentMillis = millis();
-  float lfoPeriod = 5000 / lfoFrequency;  // LFO-Periode in Millisekunden
+
+  //float lfoPeriod = 5000 / lfoFrequency;  // LFO-Periode in Millisekunden
+  float lfoPeriod = 5000 / 50;  // LFO-Periode in Millisekunden
+  //debugFloat(schrittweite);
   static float triggeredLfoValue = 0;
 
   
@@ -119,7 +133,7 @@ float calculateLFOWave(float lfoFrequency, float lfoAmplitude) {
       case SQUARE:
         
         
-        lfoValue += 0.05 * lfoDirection;  // Schrittweite für den LFO (kann angepasst werden)
+        lfoValue += schrittweite * lfoDirection;  // Schrittweite für den LFO (kann angepasst werden)
         if (lfoValue >= 1.0 || lfoValue <= 0.0) {
           lfoDirection = -lfoDirection;  // Richtung umkehren
         }
@@ -127,13 +141,13 @@ float calculateLFOWave(float lfoFrequency, float lfoAmplitude) {
        
         break;
       case TRIANGLE:
-        lfoValue += 0.01 * lfoDirection;  // Schrittweite für den LFO (kann angepasst werden)
+        lfoValue += schrittweite * lfoDirection;  // Schrittweite für den LFO (kann angepasst werden)
         if (lfoValue >= 1.0 || lfoValue <= 0.0) {
           lfoDirection = -lfoDirection;  // Richtung umkehren
         }
         break;
       case SAWTOOTH:
-        lfoValue += 0.025;  // Schrittweite für den LFO (kann angepasst werden)
+        lfoValue += schrittweite / 2;  // Schrittweite für den LFO (kann angepasst werden)
         if (lfoValue >= 1.0) {
           lfoValue = 0;  // Zurücksetzen
         }
@@ -148,7 +162,7 @@ float calculateLFOWave(float lfoFrequency, float lfoAmplitude) {
   }else{
     lfovalue_final = 1.0 + (triggeredLfoValue * (lfoAmplitude/100));
   }
-  debug(String(lfovalue_final));
+  //debug(String(lfovalue_final));
   return lfovalue_final;
 }
 
@@ -205,14 +219,19 @@ void loop() {
     previousMillis = millis();
   }
   
-  // valPitch = analogRead(freqPotPin);
+  //valPitch = analogRead(freqPotPin);
   pitchAverage();
+  //debug(String(valPitch));
 
   int lfoFreqValue = analogRead(lfoFreqPotPin);
   int lfoAmpValue = analogRead(lfoAmpPotPin);
 
-  lfoFrequency = map(lfoFreqValue, 4, 1023, 1, 50);   // LFO-Frequenzbereich von 1 Hz bis 30 Hz
-  lfoAmplitude = map(lfoAmpValue, 4, 1023, 0, 200);   // LFO-Amplitudenbereich von 0 bis 100
+  //lfoFrequency = mapFloat(lfoFreqValue, 5, 1023, 1, 50);   // LFO-Frequenzbereich von 1 Hz bis 30 Hz
+
+  lfoFrequency = map(lfoFreqValue, 5, 1023, 1, 50);   // LFO-Frequenzbereich von 1 Hz bis 30 Hz
+
+
+  lfoAmplitude = mapFloat(lfoAmpValue, 5, 1023, -100, 100);   // LFO-Amplitudenbereich von 0 bis 100
 
   if (( digitalRead(waveFormPin_0) == 0 )&&( digitalRead(waveFormPin_1) == 1 )){
     lfoWaveform = TRIANGLE;
@@ -230,7 +249,7 @@ void loop() {
   // int baseFrequency = map(valPitch, 0, 1023, 50, 4000); // direkte Ausgabe der Frequenz
   
   // Serial.println(mapFloat(valPitch, 0, 1023, 0, 100));
-  //int baseFrequency = linearToLogarithmic( mapFloat(valPitch, 0, 1023, 0, 100) );
+  //int baseFrequency =  mapFloat(valPitch, 0, 1023, 0, 100) ;
   int baseFrequency =  mapFloat(valPitch, 0, 1023, 20, 8000);
   
   // Berechne die modulierte Frequenz
@@ -238,7 +257,7 @@ void loop() {
   
   // Modulierte Frequenz berechnen
    float newModulatedFrequency = baseFrequency * calculateLFOWave(lfoFrequency, lfoAmplitude);
-
+  //debugFloat(newModulatedFrequency);
   
   // Berechne den PWM-Wert für die modulierte Frequenz
 
@@ -246,6 +265,8 @@ void loop() {
   
   // float pwm_val = setFrequency(baseFrequency); // 4140 = 500hz; 2070 = 1khz; 1035 = 2khz; 
   float pwm_val = setFrequency(newModulatedFrequency);
+  
+  
   // Setze die PWM-Periode
   // Serial.println(pwm_val);
 
@@ -261,5 +282,5 @@ void loop() {
     pwm_set_chan_level(slice_num, pwm_gpio_to_channel(PIN), pwm_val / 2);
   }
 
-  delay(1);  // Kurze Pause, um den Ton glatter zu machen
+  //delay(1);  // Kurze Pause, um den Ton glatter zu machen
 }
